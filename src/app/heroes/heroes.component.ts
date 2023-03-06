15df482fc77114db471ab8f6b2d38bac73ad6dbe
Mike van Oorschot <michael.oorschot@codurance.com>
2023-03-06 13:21:52 +0000

    Completed "Add services" section.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import {HeroService} from "../hero.service";
+import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -12,7 +13,12 @@ export class HeroesComponent {
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) {
+  constructor(private heroService: HeroService,
+              private messageService: MessageService) {
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 
   getHeroes(): void {
@@ -20,11 +26,8 @@ export class HeroesComponent {
       .subscribe(heroes => this.heroes = heroes);
   }
 
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
