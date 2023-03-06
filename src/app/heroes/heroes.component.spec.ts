2fc397cbae974ddbb88e904f49801d7e28f28de5
Mike van Oorschot <michael.oorschot@codurance.com>
2023-03-06 13:32:33 +0000

    Corrected existing unit tests. Did not expand the features tested.

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeroesComponent } from './heroes.component';
+import {HeroDetailComponent} from "../hero-detail/hero-detail.component";
 
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
@@ -8,7 +9,10 @@ describe('HeroesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeroesComponent ]
+      declarations: [
+        HeroesComponent,
+        HeroDetailComponent
+      ]
     })
     .compileComponents();
 
